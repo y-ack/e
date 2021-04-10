2d2678ce900da958b1474735faafc44e28c7ba0d
12Me21 <12Me21.mc@gmail.com>
2021-04-10 13:06:32 -0400

    switch indents to tabs

--- build.rs
+++ build.rs
@@ -1,13 +1,13 @@
 use std::path::PathBuf;
 
 fn main() {
-    // get directory of treesitter backend
-    let dir: PathBuf = ["lang", "tree-sitter-javascript", "src"].iter().collect();
-
-    // build the treesitter backend
-    cc::Build::new()
-        .include(&dir)
-        .file(dir.join("parser.c"))
-        .file(dir.join("scanner.c"))
-        .compile("tree-sitter-javascript");
+	// get directory of treesitter backend
+	let dir: PathBuf = ["lang", "tree-sitter-javascript", "src"].iter().collect();
+	
+	// build the treesitter backend
+	cc::Build::new()
+		.include(&dir)
+		.file(dir.join("parser.c"))
+		.file(dir.join("scanner.c"))
+		.compile("tree-sitter-javascript");
 }
